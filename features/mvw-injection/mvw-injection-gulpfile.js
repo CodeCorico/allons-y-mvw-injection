5b563cdf274f89fc76b57314e083baa6a409b3c9
Xavier Boubert <xavier@boubert.fr>
2016-10-19 11:53:57 +0200

    fix(mvw-injection gulpfile): add relative path to $gulp.src()

--- features/mvw-injection/mvw-injection-gulpfile.js
+++ features/mvw-injection/mvw-injection-gulpfile.js
@@ -4,15 +4,15 @@ module.exports = function($gulp) {
 
   $gulp.task('mvwInjection', function(done) {
     $gulp.src([
-      'node_modules/mvw-injection/dist/dependency-injection.js',
-      'node_modules/mvw-injection/dist/dependency-injection.min.js',
-      'node_modules/mvw-injection/dist/dependency-injection.min.js.map',
-      'node_modules/mvw-injection/dist/mvc-injection.js',
-      'node_modules/mvw-injection/dist/mvc-injection.min.js',
-      'node_modules/mvw-injection/dist/mvc-injection.min.js.map',
-      'node_modules/mvw-injection/dist/mvvm-injection.js',
-      'node_modules/mvw-injection/dist/mvvm-injection.min.js',
-      'node_modules/mvw-injection/dist/mvvm-injection.min.js.map'
+      './node_modules/mvw-injection/dist/dependency-injection.js',
+      './node_modules/mvw-injection/dist/dependency-injection.min.js',
+      './node_modules/mvw-injection/dist/dependency-injection.min.js.map',
+      './node_modules/mvw-injection/dist/mvc-injection.js',
+      './node_modules/mvw-injection/dist/mvc-injection.min.js',
+      './node_modules/mvw-injection/dist/mvc-injection.min.js.map',
+      './node_modules/mvw-injection/dist/mvvm-injection.js',
+      './node_modules/mvw-injection/dist/mvvm-injection.min.js',
+      './node_modules/mvw-injection/dist/mvvm-injection.min.js.map'
     ])
       .pipe($gulp.dist('vendor'))
       .on('end', done);
